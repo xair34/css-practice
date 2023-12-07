9827c14a7121686875ae92b260a8dfc168ea3f5a
xair34 <48828963+xair34@users.noreply.github.com>
2023-12-07 10:34:45 +0200

    Added rule sheet
    Added functionality and styling to display and hide the rule sheet for desktop view. Adjusted the selectable options visualization

---
+++ frontend_mentor_challenges/rock-paper-scissors-master/script.js
@@ -0,0 +1,18 @@
+$(function(){
+    let ruleSheetElement = $('.container-rules');
+    let overlayElement = $('.overlay');
+    $('.button-rules').on('click',function(){
+        if(!ruleSheetElement.hasClass('visible-rules')){
+            ruleSheetElement.addClass('visible-rules');
+            overlayElement.addClass('overlay-visible')
+        }
+        else{
+            overlayElement.removeClass('overlay-visible');
+            ruleSheetElement.removeClass('visible-rules');
+        }
+    })
+    $('.close-rule-sheet').on('click', function(){
+        ruleSheetElement.removeClass('visible-rules');
+        overlayElement.removeClass('overlay-visible');
+    })
+})
